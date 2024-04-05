585c1d7f68202dbe5052e47a30f314845ea16336
KaterynaKuzmenkoo <131243749+KaterynaKuzmenkoo@users.noreply.github.com>
2024-04-05 12:41:38 +0200

    Add files via upload

---
+++ uzytkownicy.js
@@ -0,0 +1,23 @@
+const uzytkownicy =[{
+    "name":"user1",
+    "email":"hutrry@gmail.com",
+    "password":"1111"
+},
+{
+    "name":"user2",
+    "email":"3dfrry@gmail.com",
+    "password":"2222"
+},
+{
+    "name":"user3",
+    "email":"68ry@gmail.com",
+    "password":"3333"
+},
+{
+    "name":"user4",
+    "email":"854ry@gmail.com",
+    "password":"4444"
+},
+]
+
+module.exports = {uzytkownicy}
